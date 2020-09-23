ea6e61d05fb5c2953c449b6c1391e1b103924281
angel <angel@mowoot.com>
2020-09-24 00:20:41 +0200

    Fase1 exercici entrada noms de ciutats amb referència dinamica a les variables String dins d'una clase de suport.

--- src/Fase1APP.java
+++ src/Fase1APP.java
@@ -1,31 +1,46 @@
+import java.lang.reflect.Field;
 import java.util.Scanner;
+
+
+class cityNames {
+	public String city_1, city_2, city_3, city_4, city_5, city_6;
+}
+
 
 public class Fase1APP {
 
-	public static void main(String[] args) {
-		
-		String city_1, city_2, city_3, city_4, city_5, city_6;
-		
-		Scanner consoleInput = new Scanner(System.in); 
-		
-		System.out.print("Entra el nom de la ciutat 1: ");
-		city_1 = consoleInput.nextLine();
-		System.out.print("Entra el nom de la ciutat 2: ");
-		city_2 = consoleInput.nextLine();
-		System.out.print("Entra el nom de la ciutat 3: ");
-		city_3 = consoleInput.nextLine();
-		System.out.print("Entra el nom de la ciutat 4: ");
-		city_4 = consoleInput.nextLine();
-		System.out.print("Entra el nom de la ciutat 5: ");
-		city_5 = consoleInput.nextLine();
-		System.out.print("Entra el nom de la ciutat 6: ");
-		city_6 = consoleInput.nextLine();
-		
-		
-		
-
-	}
 	
+	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 
+		
+		cityNames variablesCityNames = new cityNames();
+		Field field;
+		Scanner consoleInput = new Scanner(System.in); 
+
+
+		// User enters city names into String variables
+		System.out.println(" ENTER CITY NAMES\n------------------\n");
+		for (int idx = 1; idx <= 6; idx++)
+		{
+			System.out.print("Entra el nom de la ciutat " + idx + ": ");
+			field = cityNames.class.getDeclaredField("city_" + idx);
+			field.set(variablesCityNames, consoleInput.nextLine());			
+		}
+		consoleInput.close();
+		
+		
+		// Display city names entered by the user.
+		System.out.println("\n\n CITY NAMES\n------------\n");
+		for (int idx = 1; idx <= 6; idx++)
+		{
+			field = cityNames.class.getDeclaredField("city_" + idx);
+			System.out.println("ciutat_" + idx + ": " + field.get(variablesCityNames));
+						
+		}
+
+		
+	}
 
 }
+
+
