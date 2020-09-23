76298a15617dd090ba211c6e268d59b61a30bddb
angel <angel@mowoot.com>
2020-09-23 20:54:30 +0200

    Inici Fase1. Creat variables String a mà.

---
+++ src/Fase1APP.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Fase1APP {
+
+	public static void main(String[] args) {
+		
+		String city_1, city_2, city_3, city_4, city_5, city_6;
+		
+		Scanner consoleInput = new Scanner(System.in); 
+		
+		System.out.print("Entra el nom de la ciutat 1: ");
+		city_1 = consoleInput.nextLine();
+		System.out.print("Entra el nom de la ciutat 2: ");
+		city_2 = consoleInput.nextLine();
+		System.out.print("Entra el nom de la ciutat 3: ");
+		city_3 = consoleInput.nextLine();
+		System.out.print("Entra el nom de la ciutat 4: ");
+		city_4 = consoleInput.nextLine();
+		System.out.print("Entra el nom de la ciutat 5: ");
+		city_5 = consoleInput.nextLine();
+		System.out.print("Entra el nom de la ciutat 6: ");
+		city_6 = consoleInput.nextLine();
+		
+		
+		
+
+	}
+	
+
+
+}
